ba59731d625042c88678bb1d35c0460a41bd7750
mron03 <orazkhanmansur@gmail.com>
2023-10-18 19:49:28 +0600

    basic setup for authorization, now finished, just simple ui for signin and signup, did not create user class yet

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def signup(request):
+    return render(request, 'signup.html')
