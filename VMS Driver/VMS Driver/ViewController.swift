1005c483e019b1859d06397ded01fa582921a331
abitoffka <aigerim.abitayevaa@gmail.com>
2023-11-25 15:31:00 +0600

    ios app created

---
+++ VMS Driver/VMS Driver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VMS Driver
+//
+//  Created by Aigerim Abitayeva on 25.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
